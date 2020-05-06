5c7ded8d0d6974b516f9708bd6f6f39dc6fc3fba
pierrestemmett <47387020+pierrestemmett@users.noreply.github.com>
2020-05-06 21:41:58 +0200

    variable port

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');     //const = var that can't be changed
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.send("Christi you hot ey");
 });
 
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log("Christi I have something to tell you...");
 
 
